ef0ba92d6938b8ab2cb70fe41f6581f6f2bffd3b
mona <csimonusa@outlook.com>
2022-03-05 21:39:29 -0500

    folder structure/ models/ server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+const userRoute = require('')
+const authRoute = require('')
+const productRoute = require('')
+const cartRoute = require('')
+const orderRoute = require('')
+const stripeRoute = require('')
+const cors = require('cors')
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB Connection Successful!'))
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
+app.use('api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
+app.use('api/checkout', stripeRoute)
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Backend server is running!')
+})
