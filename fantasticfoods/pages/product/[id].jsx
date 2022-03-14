7546ea1c9448e852e7991842d1f3e35415e31e32
Anandneal <anandneal@gmail.com>
2022-03-14 14:39:07 -0400

    Update page to redirect to new page upon clicking product

--- fantasticfoods/pages/product/[id].jsx
+++ fantasticfoods/pages/product/[id].jsx
@@ -1,29 +1,21 @@
 import styles from "../../styles/Product.module.css";
 import Image from "next/image";
 import { useState } from "react";
+import axios from "axios";
 
-export default function Product() {
+export default function Product({ food }) {
   const [size, setSize] = useState(0);
-
-  const food1 = {
-    id: 1,
-    img: "/img/IndianFood1.PNG",
-    name: "CAMPAGNOLA",
-    price: [19.9, 23.9, 27.9],
-    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam eveniet minima quia, ad eum, blanditiis modi debitis sequi inventore maiores ipsum odio eos beatae rem nulla odit explicabo! Quod, at.",
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.imgContainer}>
-          <Image src={food1.img} objectFit="contain" layout="fill" alt="" />
+          <Image src={food.img} objectFit="contain" layout="fill" alt="" />
         </div>
       </div>
       <div className={styles.right}>
-        <h1 className={styles.title}>{food1.name}</h1>
-        <span className={styles.price}>${food1.price[size]}</span>
-        <p className={styles.desc}>{food1.desc}</p>
+        <h1 className={styles.title}>{food.title}</h1>
+        <span className={styles.price}>${food.prices[size]}</span>
+        <p className={styles.desc}>{food.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
           <div className={styles.size} onClick={() => setSize(0)}>
@@ -79,10 +71,20 @@ export default function Product() {
           </div>
         </div>
         <div className={styles.add}>
-          <input type="number" defaultValue={1} className={styles.quantity}/>
+          <input type="number" defaultValue={1} className={styles.quantity} />
           <button className={styles.button}>Add to Cart</button>
         </div>
       </div>
     </div>
   );
 }
+export const getServerSideProps = async ({ params }) => {
+  const res = await axios.get(
+    `http://localhost:3000/api/products/${params.id}`
+  );
+  return {
+    props: {
+      food: res.data,
+    },
+  };
+};
